bb8a456eed0f73a2f2d85be3537d50f3cdba4bc5
JackelineMatos <jackeline.matos@gmail.com>
2023-09-09 20:03:15 +0200

    creating-todolist

---
+++ script.js
@@ -0,0 +1,14 @@
+/* resolver problema com a importacao do documento js
+add document.addEventListener("DomContentLoad")
+
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    const newItemInput = document.getElementById
+    ("input-new-item");
+
+   const buttonAdd = document.getElementsByClassName("add-button");
+   console.log(buttonAdd)
+} 
+
+);
